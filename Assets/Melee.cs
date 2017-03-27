e70ffc3575a3be648d676e167bb620c726788d31
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-03-27 11:44:04 -0500

    Began work on the warrior

---
+++ Assets/Melee.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour 
+{
+	public KeyCode keyCode;
+
+	public static bool attacking;
+	Animator anim;
+	int melee;
+
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () 
+	{
+		if (Input.GetKey (keyCode) && !attacking && TargetObject.target != null) 
+		{
+			attacking = true;
+			StartCoroutine (Attack ());
+		}
+	}
+
+	IEnumerator Attack()
+	{
+		melee++;
+		if (melee == 4)
+			melee = 1;
+		anim.SetInteger ("Melee", melee);
+		yield return new WaitForSeconds (.3f);
+		anim.SetInteger ("Melee", 0);
+		attacking = false;
+	}
+}
---
+++ Assets/MeleeAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour 
+{
+	public int minDamage;
+	public int maxDamage;
+	public int criticalNumber;
+	int damage;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Enemy" && Melee.attacking) 
+		{
+			other.GetComponent<Health> ().TookDamage (CritChance(),damage);
+		}
+	}
+
+	bool CritChance()
+	{
+		int damageAmount = Random.Range (minDamage, maxDamage);
+		damage = damageAmount; 
+		if (damageAmount < criticalNumber)
+			return false;
+		else
+			return true;
+	}
+}
