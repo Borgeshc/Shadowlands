5ffa0ec2c4a1ce5f63552948267f578ac46cb473
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-03-31 23:40:26 -0500

    Added ArrowStorm and began work on rain of arrows

---
+++ Assets/ArrowStorm.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowStorm : MonoBehaviour 
+{
+	public KeyCode keyCode;
+	public float abilityCooldown;
+	public float animationLength;
+	public GameObject arrowstormEffect;
+	public GameObject arrowstormDamage;
+	public float resourceCost;
+	public ResourceManager resourceManager;
+
+	bool onCooldown;
+	AbilityManager abilityManager;
+	Animator anim;
+
+	void Start () 
+	{
+		abilityManager = GameObject.Find("GameManager").GetComponent<AbilityManager> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () 
+	{
+		if (Input.GetKeyDown (keyCode) && !abilityManager.abilityInProgress && resourceManager.resource > resourceCost && !onCooldown) 
+		{
+			onCooldown = true;
+			anim.SetBool ("ArrowStorm", true);
+		}
+	}
+
+	void ArrowstormAttackBegin()
+	{
+		resourceManager.CostResource (resourceCost);
+		arrowstormEffect.SetActive (true);
+	}
+
+	void ArrowstormAttackEnd()
+	{
+		arrowstormEffect.SetActive (false);
+		anim.SetBool("ArrowStorm", false);
+		abilityManager.abilityInProgress = false;
+		StartCoroutine (Cooldown ());
+	}
+
+	IEnumerator Cooldown()
+	{
+		yield return new WaitForSeconds (abilityCooldown);
+		onCooldown = false;
+	}
+	IEnumerator ArrowstormDamage()
+	{
+		arrowstormDamage.SetActive (true);
+		yield return new WaitForSeconds (.1f);
+		arrowstormDamage.SetActive (false);
+	}
+}
---
+++ Assets/RainOfArrows.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainOfArrows : MonoBehaviour 
+{
+	public KeyCode keyCode;
+	public GameObject arrowSpawn;
+	public float abilityLength;
+	public float abilityCooldown;
+	public float resourceCost;
+	public GameObject rainOfArrowsDamage;
+	public ResourceManager resourceManager;
+
+	bool spendingResource;
+	bool firing;
+	bool onCooldown;
+
+	AbilityManager abilityManager;
+	ObjectPooling rainOfArrowsPool;
+	Animator anim;
+	Vector3 tarPos;
+	Vector3 destinationPosition;
+	GameObject target;
+
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+		abilityManager = GameObject.Find ("GameManager").GetComponent<AbilityManager> ();
+		rainOfArrowsPool = GameObject.Find ("RainOfArrowsPool").GetComponent<ObjectPooling> ();
+	}
+
+	void Update () 
+	{
+		if (Input.GetKey (keyCode) && !abilityManager.abilityInProgress && TargetObject.target != null &&  resourceManager.resource > resourceCost && !firing && !onCooldown) 
+		{
+			target = TargetObject.target;
+			firing = true;
+			StartCoroutine(Fire ());
+		} 
+	}
+
+	IEnumerator Fire()
+	{
+		if (Input.GetMouseButtonDown (1) && GUIUtility.hotControl == 0) {
+
+			Plane playerPlane = new Plane (Vector3.up, transform.position);
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			float hitdist = 0.0f;
+
+			if (playerPlane.Raycast (ray, out hitdist)) {
+				Vector3 targetPoint = ray.GetPoint (hitdist);
+				destinationPosition = ray.GetPoint (hitdist);
+				Quaternion targetRotation = Quaternion.LookRotation (targetPoint - transform.position);
+				//	myTransform.rotation = Quaternion.Slerp (myTransform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+				transform.rotation = targetRotation;
+			}
+		}
+		abilityManager.abilityInProgress = true;
+		ClickToMove.canMove = false;
+		anim.SetBool ("RainOfArrows", true);
+		anim.SetBool ("RainOfArrowsDamage", true);
+
+		yield return new WaitForSeconds (abilityLength);
+
+		ClickToMove.canMove = true;
+		anim.SetBool ("RainOfArrows", false);
+		abilityManager.abilityInProgress = false;
+		firing = false;
+	}
+
+	void SpawnRainOfArrows()
+	{
+		if (!spendingResource) {
+			spendingResource = true;
+			resourceManager.CostResource (resourceCost);
+		}
+
+		GameObject obj = rainOfArrowsPool.GetPooledObject();
+
+		if (obj == null)
+		{
+			return;
+		}
+		obj.transform.position = target.transform.position + new Vector3(0, 2,0);
+		obj.transform.rotation = new Quaternion(gameObject.transform.rotation.x,-gameObject.transform.rotation.y,gameObject.transform.rotation.z,gameObject.transform.rotation.w);
+		obj.SetActive(true);
+
+		spendingResource = false;
+		onCooldown = true;
+		StartCoroutine (RainOfArrowsDamage (obj.transform.FindChild("ArrowStormDamage").gameObject));
+		StartCoroutine (Cooldown());
+	}
+
+	IEnumerator RainOfArrowsDamage(GameObject _rainOfArrowDamage)
+	{
+		rainOfArrowsDamage = _rainOfArrowDamage;
+		rainOfArrowsDamage.SetActive (true);
+		yield return new WaitForSeconds (.1f);
+		rainOfArrowsDamage.SetActive (false);
+	}
+
+	void RainOfArrowsDamageEnd()
+	{
+		if(anim.GetBool("RainOfArrowsDamage") == true)
+		anim.SetBool ("RainOfArrowsDamage", false);
+	}
+
+	IEnumerator Cooldown()
+	{
+		yield return new WaitForSeconds (abilityCooldown);
+		onCooldown = false;
+	}
+}
