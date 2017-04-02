62b189d24f63207cb9d257c10a1f767bee38cd30
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-04-02 00:42:36 -0500

    Added art for main menu

---
+++ Assets/CharacterSelected.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelected : MonoBehaviour
+{
+    public Animator anim;
+
+    public void Selected()
+    {
+        anim.SetBool("Selected", true);
+    }
+
+    public void DeSelected()
+    {
+        anim.SetBool("Selected", false);
+    }
+}
